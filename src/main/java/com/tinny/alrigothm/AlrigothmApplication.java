004c42ece8467b28ea433842a18a20de5419317b
xiajingchuan <xiajingchuan@ruijie.com.cn>
2020-03-01 13:22:12 +0800

    sss

---
+++ src/main/java/com/tinny/alrigothm/AlrigothmApplication.java
@@ -0,0 +1,13 @@
+package com.tinny.alrigothm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlrigothmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlrigothmApplication.class, args);
+    }
+
+}
