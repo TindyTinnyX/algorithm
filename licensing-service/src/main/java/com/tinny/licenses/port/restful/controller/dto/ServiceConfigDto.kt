1f0f4d4b156e2554a2c92d385f54a2b5b1313953
xiajingchuan <xiajingchuan@ruijie.com.cn>
2020-04-24 16:11:07 +0800

    提交

---
+++ licensing-service/src/main/java/com/tinny/licenses/port/restful/controller/dto/ServiceConfigDto.kt
@@ -0,0 +1,9 @@
+package com.tinny.licenses.port.restful.controller.dto
+
+/**
+ * @author : Tinny
+ * @date : Created on 2020/4/24 - 15:49
+ * @description :
+ * @modified :
+ */
+data class ServiceConfigDto(val exampleProperty: String? = null)
